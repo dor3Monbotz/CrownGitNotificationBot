c89a7c198be5a9197831a0d43bd16c2a16e0ef99
dor3Monbotz <118621248+dor3Monbotz@users.noreply.github.com>
2022-11-29 18:20:08 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# Your Bot token in the quotes taken from @BotFather
+BOT_TOKEN = "5830716619:AAFtl8A2bKi8R-yjoBSFE-5VrZCxinIF7NQ"
+
+# Project Name of your Project
+PROJECT_NAME = "CrownGitHub"
+
+# If you forked the source code, grab your repo's link and paste it below!
+GIT_REPO_URL = "https://github.com/dor3Monbotz/CrownGitNotificationBot.git"
